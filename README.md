5709623dd37b3605248746050c7f5cee481f4f1b
Alexander Fuchs <alex.fu27@gmail.com>
2021-05-21 16:51:16 +0200

    documentation

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Memory usage statistics for AVR-based Arduino boards.
 PlatformIO library.
 
+Uses internal variables of AVR-libc, so may break at some time.
+
 See [C header](include/MemoryStats.h) for available methods.
--- include/MemoryStats.h
+++ include/MemoryStats.h
@@ -11,7 +11,16 @@ extern "C" {
 
 #include <stddef.h>
 
+/*
+ * Return free memory between heap and top of stack,
+ * subtracting the malloc margin.
+ * This is how much the heap can still grow.
+ */
 size_t free_stack(void);
+
+/*
+ * Amount of memory that is currently allocated by the heap.
+ */
 size_t allocated_heap(void);
 
 #ifdef __cplusplus
